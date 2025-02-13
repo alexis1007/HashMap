2a1f2eedbfaa426efd6306f8b6b5a7365ece2690
Alexis1007 <alexis.1007@hotmail.com>
2025-02-13 11:44:26 -0600

    3

--- src/App.java
+++ src/App.java
@@ -4,17 +4,14 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        HashMap<String, String> names = new HashMap<String, String>();
+        HashMap<Integer, String> names = new HashMap<Integer, String>();
 
-        names.put("1", "Roberto");
-        names.put("2", "Rodrigo");   
-        names.put("3", "Rafael");
+        names.put(1, "Roberto");
+        names.put(2, "Rodrigo");   
+        names.put(3, "Rafael");
 
-        //System.out.println(names);
-
-        for (Map.Entry<String, String> entry : names.entrySet()) {
+        for (Map.Entry<Integer, String> entry : names.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
         }
     }
