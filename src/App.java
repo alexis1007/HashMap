40cd110d65814e0d2c75ed9fa3bdb6c3b3706a22
Alexis1007 <alexis.1007@hotmail.com>
2025-02-13 12:02:18 -0600

    4

--- src/App.java
+++ src/App.java
@@ -1,18 +1,34 @@
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        HashSet<String> ages = new HashSet<String>();
         HashMap<Integer, String> names = new HashMap<Integer, String>();
 
+        //HashMap Names
         names.put(1, "Roberto");
-        names.put(2, "Rodrigo");   
+        names.put(2, "Rodrigo");
         names.put(3, "Rafael");
+
+        //HashSet ages
+        ages.add("20");
+        ages.add("30");
+        ages.add("40");
 
         for (Map.Entry<Integer, String> entry : names.entrySet()) {
             System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
         }
+
+        int c=1;
+        for(String age : ages){
+            System.out.println("The age of "+c + " is " + age);
+            c++;
+        }
+
+
     }
 }
