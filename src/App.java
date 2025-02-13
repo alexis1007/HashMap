7477a3a8f494cb8d42c6cd78b07ccd2996c4f904
Alexis1007 <alexis.1007@hotmail.com>
2025-02-13 11:41:08 -0600

    2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        HashMap<String, String> names = new HashMap<String, String>();
+
+        names.put("1", "Roberto");
+        names.put("2", "Rodrigo");   
+        names.put("3", "Rafael");
+
+        //System.out.println(names);
+
+        for (Map.Entry<String, String> entry : names.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+        }
     }
 }
